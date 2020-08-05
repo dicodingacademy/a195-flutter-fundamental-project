b9041286f60e33d40e2ecd3007c8d7f4efaa6c80
Widyarso Joko Purnomo <widyjp10@gmail.com>
2020-08-05 16:50:36 +0700

    initial commit

---
+++ android/app/src/main/kotlin/dev/widdyjp/dicoding_news_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.widdyjp.dicoding_news_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: NewsListPage(),
+    );
+  }
+}
+
+class NewsListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
