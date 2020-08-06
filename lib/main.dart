1e106146bd9a507f03dbfdb81b72fa067a7cc43d
Widyarso Joko Purnomo <widyjp10@gmail.com>
2020-08-06 11:00:47 +0700

    add article list

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dicoding_news_app/article.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -21,6 +22,28 @@ class MyApp extends StatelessWidget {
 class NewsListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('News App'),
+      ),
+      body: ListView(
+        children: getArticlesJson.map((json) {
+          return _buildArticleItem(Article.fromJson(json));
+        }).toList(),
+      ),
+    );
+  }
+
+  Widget _buildArticleItem(Article article) {
+    return ListTile(
+      contentPadding:
+          const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      leading: Image.network(
+        article.urlToImage,
+        width: 100,
+      ),
+      title: Text(article.title),
+      subtitle: Text(article.author),
+    );
   }
 }
