5adcb336ad68f824a6e6916249974114f23b30a7
rrifafauzikomara <rifafauzi6@gmail.com>
2020-08-22 00:11:37 +0700

    add provider class

--- lib/provider/news_provider.dart
+++ lib/provider/news_provider.dart
@@ -1,3 +1,23 @@
+import 'dart:async';
+
+import 'package:dicoding_news_app/data/api/api_service.dart';
+import 'package:dicoding_news_app/data/model/articles.dart';
 import 'package:flutter/material.dart';
 
-class NewsProvider extends ChangeNotifier {}
+class NewsProvider extends ChangeNotifier {
+  final _apiService = ApiService();
+  final _streamController = StreamController<ArticlesResult>();
+
+  Stream<ArticlesResult> get catPhotoStream => _streamController.stream;
+
+  fetchAllArticle(String mealsType) async {
+    final article = await _apiService.topHeadlines();
+    _streamController.sink.add(article);
+  }
+
+  @override
+  void dispose() {
+    _streamController.close();
+    super.dispose();
+  }
+}
