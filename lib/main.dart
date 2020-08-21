445da6488278cb33e24a829b1f25b1ad946aa39c
rrifafauzikomara <rifafauzi6@gmail.com>
2020-08-21 16:21:37 +0700

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,22 @@
-import 'package:dicoding_news_app/article.dart';
+import 'package:dicoding_news_app/ui/home_page.dart';
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  static const title = 'News App';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'News App',
+      title: title,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: NewsListPage(),
-    );
-  }
-}
-
-class NewsListPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('News App'),
-      ),
-      body: FutureBuilder<String>(
-        future:
-            DefaultAssetBundle.of(context).loadString('assets/articles.json'),
-        builder: (context, snapshot) {
-          final List<Article> articles = parseArticles(snapshot.data);
-          return ListView(
-            children: articles
-                .map((article) => _buildArticleItem(context, article))
-                .toList(),
-          );
-        },
-      ),
-    );
-  }
-
-  Widget _buildArticleItem(BuildContext context, Article article) {
-    return ListTile(
-      contentPadding:
-          const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      leading: Image.network(
-        article.urlToImage,
-        width: 100,
-      ),
-      title: Text(article.title),
-      subtitle: Text(article.author),
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => ArticleDetailPage(url: article.url),
-          ),
-        );
-      },
-    );
-  }
-}
-
-class ArticleDetailPage extends StatelessWidget {
-  final String url;
-
-  ArticleDetailPage({@required this.url});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('News App'),
-      ),
-      body: WebView(
-        initialUrl: url,
-      ),
+      home: HomePage(title: title),
     );
   }
 }
