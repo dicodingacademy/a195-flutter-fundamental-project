f4e8a6950ac36cc195c59fa26b4db21dfeb5cb08
R Rifa Fauzi Komara <rifafauzi6@gmail.com>
2020-08-24 15:09:20 +0700

    fixed model

--- lib/data/model/articles.dart
+++ lib/data/model/articles.dart
@@ -1,23 +1,34 @@
 class ArticlesResult {
+  ArticlesResult({
+    this.status,
+    this.totalResults,
+    this.articles,
+  });
+
   String status;
   int totalResults;
   List<Articles> articles;
 
-  ArticlesResult({this.status, this.totalResults, this.articles});
-
-  ArticlesResult.fromJson(Map<String, dynamic> json) {
-    status = json['status'];
-    totalResults = json['totalResults'];
-    if (json['articles'] != null) {
-      articles = new List<Articles>();
-      json['articles'].forEach((v) {
-        articles.add(new Articles.fromJson(v));
-      });
-    }
-  }
+  factory ArticlesResult.fromJson(Map<String, dynamic> json) => ArticlesResult(
+        status: json["status"],
+        totalResults: json["totalResults"],
+        articles: List<Articles>.from(
+            json["articles"].map((x) => Articles.fromJson(x))),
+      );
 }
 
 class Articles {
+  Articles({
+    this.source,
+    this.author,
+    this.title,
+    this.description,
+    this.url,
+    this.urlToImage,
+    this.publishedAt,
+    this.content,
+  });
+
   Source source;
   String author;
   String title;
@@ -27,37 +38,29 @@ class Articles {
   String publishedAt;
   String content;
 
-  Articles(
-      {this.source,
-      this.author,
-      this.title,
-      this.description,
-      this.url,
-      this.urlToImage,
-      this.publishedAt,
-      this.content});
-
-  Articles.fromJson(Map<String, dynamic> json) {
-    source =
-        json['source'] != null ? new Source.fromJson(json['source']) : null;
-    author = json['author'];
-    title = json['title'];
-    description = json['description'];
-    url = json['url'];
-    urlToImage = json['urlToImage'];
-    publishedAt = json['publishedAt'];
-    content = json['content'];
-  }
+  factory Articles.fromJson(Map<String, dynamic> json) => Articles(
+        source: Source.fromJson(json["source"]),
+        author: json["author"],
+        title: json["title"],
+        description: json["description"],
+        url: json["url"],
+        urlToImage: json["urlToImage"],
+        publishedAt: json["publishedAt"],
+        content: json["content"] == null ? null : json["content"],
+      );
 }
 
 class Source {
-  String id;
+  Source({
+    this.id,
+    this.name,
+  });
+
+  int id;
   String name;
 
-  Source({this.id, this.name});
-
-  Source.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    name = json['name'];
-  }
+  factory Source.fromJson(Map<String, dynamic> json) => Source(
+        id: json["id"],
+        name: json["name"],
+      );
 }
