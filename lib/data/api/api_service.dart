36d6fffeadc343a9c27aff2d2c7c4dbcb957a35a
rrifafauzikomara <rifafauzi6@gmail.com>
2020-08-25 18:53:09 +0700

    fixed service

--- lib/data/api/api_service.dart
+++ lib/data/api/api_service.dart
@@ -1,19 +1,15 @@
-import 'dart:async';
 import 'dart:convert';
-
+import 'package:http/http.dart' as http;
 import 'package:dicoding_news_app/data/model/articles.dart';
-import 'package:http/http.dart' show Client;
 
 class ApiService {
-  Client _client = Client();
-
   static final String _baseUrl = 'https://newsapi.org/v2/';
   static final String _apiKey = '81d98da5c83d45a5ad24b6ab1698e745';
   static final String _category = 'business';
   static final String _country = 'id';
 
   Future<ArticlesResult> topHeadlines() async {
-    final response = await _client.get(_baseUrl +
+    final response = await http.get(_baseUrl +
         "top-headlines?country=$_country&category=$_category&apiKey=$_apiKey");
     if (response.statusCode == 200) {
       return ArticlesResult.fromJson(json.decode(response.body));
