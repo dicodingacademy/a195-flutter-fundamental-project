038b35b83e26401f47f509f507e549833b42cc41
Widyarso Joko Purnomo <widyjp10@gmail.com>
2020-08-25 10:43:43 +0700

    rename file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:dicoding_news_app/article.dart';
-import 'package:dicoding_news_app/article_detail.dart';
+import 'package:dicoding_news_app/detail_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
