e259ccbbbeefefc6aae18d2a045aeedfdab786f4
rrifafauzikomara <rifafauzi6@gmail.com>
2020-09-10 00:50:57 +0700

    refactoring

--- lib/common/navigation.dart
+++ lib/common/navigation.dart
@@ -3,10 +3,6 @@ import 'package:flutter/material.dart';
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 class Navigation {
-  static intent(String routeName) {
-    navigatorKey.currentState.pushNamed(routeName);
-  }
-
   static intentWithData(String routeName, Object arguments) {
     navigatorKey.currentState.pushNamed(routeName, arguments: arguments);
   }
