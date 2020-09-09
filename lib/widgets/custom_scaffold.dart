53f3b2928f6aa1b24f095babd36c001556a31a5a
Widyarso Joko Purnomo <widyjp10@gmail.com>
2020-09-09 11:50:09 +0700

    change class to method

--- lib/widgets/custom_scaffold.dart
+++ lib/widgets/custom_scaffold.dart
@@ -12,7 +12,7 @@ class CustomScaffold extends StatelessWidget {
         child: Stack(
           children: [
             body,
-            ShortAppBar(),
+            _buildShortAppBar(context),
           ],
         ),
       ),
@@ -20,34 +20,31 @@ class CustomScaffold extends StatelessWidget {
   }
 }
 
-class ShortAppBar extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(0),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 16.0),
-            child: Text(
-              'N',
-              style: Theme.of(context).textTheme.headline6,
-            ),
-          ),
-        ],
-      ),
-      shape: BeveledRectangleBorder(
-        borderRadius: BorderRadius.only(
-          bottomRight: Radius.circular(16.0),
+Widget _buildShortAppBar(BuildContext context) {
+  return Card(
+    margin: EdgeInsets.all(0),
+    child: Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
         ),
+        Padding(
+          padding: const EdgeInsets.only(right: 16.0),
+          child: Text(
+            'N',
+            style: Theme.of(context).textTheme.headline6,
+          ),
+        ),
+      ],
+    ),
+    shape: BeveledRectangleBorder(
+      borderRadius: BorderRadius.only(
+        bottomRight: Radius.circular(16.0),
       ),
-    );
-  }
+    ),
+  );
 }
