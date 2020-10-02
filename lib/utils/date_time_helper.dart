39c904b917e8196c670c9b76c2e81044ce7b824c
rrifafauzikomara <rifafauzi6@gmail.com>
2020-10-02 11:29:01 +0700

    add condition for scheduling news

--- lib/utils/date_time_helper.dart
+++ lib/utils/date_time_helper.dart
@@ -2,13 +2,29 @@ import 'package:intl/intl.dart';
 
 class DateTimeHelper {
   static DateTime format() {
+    // Date and Time Format
     final now = DateTime.now();
     final dateFormat = DateFormat('y/M/d');
     final timeSpecific = "08:00:00";
-    final todayDate = dateFormat.format(now);
-    final completeDateAndTime = "$todayDate $timeSpecific";
     final completeFormat = DateFormat('y/M/d h:m:s');
-    var result = completeFormat.parseStrict(completeDateAndTime);
-    return result;
+
+    // Today Format
+    final todayDate = dateFormat.format(now);
+    final todayDateAndTime = "$todayDate $timeSpecific";
+    var resultToday = completeFormat.parseStrict(todayDateAndTime);
+
+    // Tomorrow Format
+    var formatted = resultToday.add(Duration(days: 1));
+    final tomorrowDate = dateFormat.format(formatted);
+    final tomorrowDateAndTime = "$tomorrowDate $timeSpecific";
+    var resultTomorrow = completeFormat.parseStrict(tomorrowDateAndTime);
+
+    if (now.millisecondsSinceEpoch > resultToday.millisecondsSinceEpoch) {
+      print('Tomorrow --> $resultTomorrow');
+      return resultTomorrow;
+    } else {
+      print('Today --> $resultToday');
+      return resultToday;
+    }
   }
 }
