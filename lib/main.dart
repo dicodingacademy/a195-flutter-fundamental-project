1d03fcf0606474a1f519ca8c92f7a42bcc553d9f
Achmad Ilham <achmadilham07@gmail.com>
2021-12-22 13:08:20 +0700

    update ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -15,15 +15,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'News App',
       theme: ThemeData(
-        primaryColor: primaryColor,
-        accentColor: secondaryColor,
+        colorScheme: Theme.of(context).colorScheme.copyWith(
+              primary: primaryColor,
+              onPrimary: Colors.black,
+              secondary: secondaryColor,
+            ),
         scaffoldBackgroundColor: Colors.white,
         visualDensity: VisualDensity.adaptivePlatformDensity,
         textTheme: myTextTheme,
-        appBarTheme: AppBarTheme(
-          textTheme: myTextTheme.apply(bodyColor: Colors.black),
-          elevation: 0,
-        ),
+        appBarTheme: AppBarTheme(elevation: 0),
         bottomNavigationBarTheme: BottomNavigationBarThemeData(
           selectedItemColor: secondaryColor,
           unselectedItemColor: Colors.grey,
@@ -31,6 +31,7 @@ class MyApp extends StatelessWidget {
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
             primary: secondaryColor,
+            onPrimary: Colors.white,
             textStyle: TextStyle(),
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.all(
